85f504bb54ea1b80eafd2a83396ed9cfa8f9a8d3
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-07-15 18:29:16 +1000

    create add_prefix function and successfully tested adding prefix to all files in folder

--- rename.py
+++ rename.py
@@ -1,4 +1,6 @@
 import os
+from glob import glob
+import shutil
 
 current_folder = os.path.dirname(__file__)
 files_folder = os.path.join(current_folder, "files")
@@ -18,14 +20,37 @@ def create_test_files(folder=files_folder, file_count=10):
         print("Done!")
 
 
-def replace_prefix(folder=files_folder):
-    pass
+def add_prefix(folder=files_folder):
+    print("Please enter the prefix to be added to the files: ")
+    prefix = input()
+    print("Please enter the pattern of files that will have this prefix (eg *.txt): ")
+    pattern = input()
+
+    os.chdir(folder)
+
+    print(f"Adding prefix '{prefix}' to all files...")
+    [os.rename(f, f"{prefix}{f}") for f in glob(pattern)]
+    print("Done!")
+
+
+def delete_old_files(folder=files_folder):
+    print(
+        "We have now renamed your files. Would you like to delete your previous files? (Y/N)"
+    )
+    delete_response = input()
+
+    if delete_response.lower() == "yes" or delete_response.lower() == "y":
+        print("deleting...")
+    else:
+        print("no worries!")
 
 
 def main():
     ### comment out the `create_test_files()` function if you do not need to create test files ###
     # create_test_files()
-    pass
+    # print(f"Please place all files in: {folder}.")
+    add_prefix()
+    delete_old_files()
     # print(os.path.dirname(__file__))
 
 
