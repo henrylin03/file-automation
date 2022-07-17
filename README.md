bf0c93a76321eaba08449b20b27837b24eccd7f5
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-07-18 08:46:06 +1000

    update README with instructions on backing up previous files; add backup_files function and successfully test; add to delete_backup_files function

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Rename
-In this project, I use Python to rename files in a designated folder to avoid having to do it manually.
+In this project, I use Python to quickly rename files in a designated folder.
+
+## How to Use
+1. Download the `rename.py` code and place in the folder with the files you want to rename.
+2. Run the code and follow the prompts.
+3. The code creates a folder called `/prev_files` that contains the files before renaming. This allows you to restore the files if required.
+
+If you would like to test the code, the `create_test_files()` function will generate test .txt files in the `/files` folder for you to manipulate.
+
+## Issues/Questions/Enhancements?
+If you have any questions, experienced any issues, and/or have any suggestions for enhancements, please raise a GitHub Issue and tag it. Thank you for your support!
--- rename.py
+++ rename.py
@@ -1,12 +1,64 @@
 import os
 from glob import glob
+from pathlib import Path
 import shutil
 
 current_folder = os.path.dirname(__file__)
-files_folder = os.path.join(current_folder, "files")
+backup_folder = os.path.join(current_folder, "prev_files")
+test_files_folder = os.path.join(current_folder, "files")
 
 
-def create_test_files(folder=files_folder, file_count=10):
+def backup_files(files_list, folder=current_folder):
+    print("Backing up current files to the /prev_files folder...")
+
+    Path(backup_folder).mkdir(parents=True, exist_ok=True)
+
+    destination = backup_folder
+    for f in files_list:
+        source = os.path.join(folder, f)
+        shutil.copy2(source, destination)
+    print("Done!")
+
+
+### this function is incomplete - may need to alter the add_prefix function to create a copy instead
+def delete_backup_files(folder=backup_folder):
+    while True:
+        print(
+            "We have now renamed your files. Would you like to delete your previous files? (Y/N)"
+        )
+        delete_response = input()
+
+        if delete_response.lower() == "yes" or delete_response.lower() == "y":
+            print("Deleting previous files...")
+            backup_files = glob(folder)
+            for f in backup_files:
+                os.remove(f)
+            ## need to debug "PermissionError [WinError5]" - I think this is if you have the folder open!
+            break
+        elif delete_response.lower() == "no" or delete_response.lower() == "n":
+            print("Previous files are kept in the /prev_files folder.")
+            break
+        else:
+            print("Sorry, I did not understand. Please try again.\n")
+
+
+def add_prefix(folder=current_folder):
+    print("Please enter the prefix to be added to the files: ")
+    prefix = input()
+    print("Please enter the pattern of files that will have this prefix (eg *.txt): ")
+    pattern = input()
+
+    os.chdir(folder)
+
+    backup_files(glob(pattern), folder)
+
+    print(f"Adding prefix '{prefix}' to all files...")
+    [os.rename(f, f"{prefix}{f}") for f in glob(pattern)]
+    print("Done!")
+    ## need to add debugging for "File already exists error" when the final, renamed file already exists!
+
+
+def create_test_files(folder=test_files_folder, file_count=10):
     print("Press any key to create test files. Type 'skip' if you would like to skip.")
     test_input = input()
 
@@ -28,41 +80,12 @@ def create_test_files(folder=files_folder, file_count=10):
             print("Done!")
 
 
-def add_prefix(folder=current_folder):
-    print("Please enter the prefix to be added to the files: ")
-    prefix = input()
-    print("Please enter the pattern of files that will have this prefix (eg *.txt): ")
-    pattern = input()
-
-    os.chdir(folder)
-
-    print(f"Adding prefix '{prefix}' to all files...")
-    [os.rename(f, f"{prefix}{f}") for f in glob(pattern)]
-    print("Done!")
-
-
-### this function is incomplete - may need to alter the add_prefix function to create a copy instead
-def delete_old_files(folder=files_folder):
-    while True:
-        print(
-            "We have now renamed your files. Would you like to delete your previous files? (Y/N)"
-        )
-        delete_response = input()
-
-        if delete_response.lower() == "yes" or delete_response.lower() == "y":
-            print("Deleting previous files...")
-            break
-        elif delete_response.lower() == "no" or delete_response.lower() == "n":
-            print("Previous files are kept in the same folder")
-            break
-        else:
-            print("Sorry, I did not understand. Please try again.")
-
-
 def main():
-    create_test_files()
+    print("Press any key to begin!")
+    create_test_files()  ## need to consider how to ensure that code is used for prod differently than if it were to be used for testing -- we don't really even need this function in main() then!
     # print(f"Please place all files in: {folder}.")
-    add_prefix(files_folder)
+    add_prefix(test_files_folder)
+    delete_backup_files()
 
 
 if __name__ == "__main__":
